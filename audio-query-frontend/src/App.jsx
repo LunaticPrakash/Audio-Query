5268efcd12580aa01527d0ca1734c1ef1a38da16
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2025-09-04 18:27:42 +0530

    Update App.jsx

--- audio-query-frontend/src/App.jsx
+++ audio-query-frontend/src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
     { "lang": "French", "code": "fr" },
   ];
 
-  const BASE_URL = "https://61756944feb1.ngrok-free.app";
+  const BASE_URL = "https://464f275f1e53.ngrok-free.app";
 
   const checkBackendStatus = async () => {
     try {
